8796fd14c0570ec3c9f3317b31769a45f5d6027f
WizzyCodes <wisdomiberi832@gmail.com>
2024-11-20 00:04:04 +0100

    First day

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 console.log("welcome to loop collaboration");
-// import log from "console";
-// import clear from "console";
-// console.log("hello");
+import log from "console";
+import clear from "console";
+console.log("hello");
 
 //for loop
 for (let i = 1; i < 100; i += 2) {
   console.log(i);
 }
-// console.clear();
+console.clear();
 
-//while loop
-// let i = 0;
-// while (i < 100) {
-//   console.log(i);
-//   i++;
-// }
+let i = 0;
+while (i < 100) {
+  console.log(i);
+  i++;
+}
