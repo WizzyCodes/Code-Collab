6467454db4026b494fbcd189d502741efe3b0e77
WizzyCodes <wisdomiberi832@gmail.com>
2024-11-20 00:03:09 +0100

    First day

--- index.ts
+++ index.ts
@@ -1,17 +1,17 @@
-// console.log("welcome to loop collaboration");
-// for (let i = 1; i < 100; i += 2) {
-//   console.log(i);
-// }
+console.log("welcome to loop collaboration");
+for (let i = 1; i < 100; i += 2) {
+  console.log(i);
+}
 console.clear();
-// const x1 = setInterval(() => {
-//   for (let i = 0; i < 5; i++) console.log(i);
-//   //   clearInterval(x);
-// }, 3000);
+const x1 = setInterval(() => {
+  for (let i = 0; i < 5; i++) console.log(i);
+  //   clearInterval(x);
+}, 3000);
 
-// setTimeout(() => {
-//   clearInterval(x1);
-//   console.log("Interval cleared");
-// }, 10000);
+setTimeout(() => {
+  clearInterval(x1);
+  console.log("Interval cleared");
+}, 10000);
 
 let count = 0;
 const x = setInterval(() => {
